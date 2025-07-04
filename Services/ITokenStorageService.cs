c40c8ebd0c0ef9308696664b6956b14c9c6dccc9
Benjamin Belanger <99574524+BenjaminBelanger@users.noreply.github.com>
2025-07-04 00:27:14 -0400

    Implement token storage (#13)
    * Add token storage
    
    * Add refresh token storage
    
    * Simplify code
    
    * Implement ITokenStorageService interface
    
    * Add token saving after user login

---
+++ Services/ITokenStorageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_sync_frontend.Services
+{
+    public interface ITokenStorageService
+    {
+        void SaveTokens(string? accessToken, string? refreshToken);
+        (string? AccessToken, string? RefreshToken) GetTokens();
+        void DeleteTokens();
+    }
+}
---
+++ Services/TokenStorageService.cs
@@ -0,0 +1,60 @@
+﻿using CredentialManagement;
+using System;
+
+namespace music_sync_frontend.Services
+{
+    public class TokenStorageService : ITokenStorageService
+    {
+        private const string AccessTokenKey = "Spotify_Access_Token";
+        private const string RefreshTokenKey = "Spotify_Refresh_Token";
+
+        public void SaveTokens(string? accessToken, string? refreshToken)
+        {   
+            if (accessToken != null)
+            {
+                SaveToken(AccessTokenKey, accessToken);
+            }
+            if (refreshToken != null) {
+                SaveToken(RefreshTokenKey, refreshToken);
+            }
+        }
+
+        public (string? AccessToken, string? RefreshToken) GetTokens()
+        {
+            return (
+                GetToken(AccessTokenKey),
+                GetToken(RefreshTokenKey)
+            );
+        }
+
+        public void DeleteTokens()
+        {
+            DeleteToken(AccessTokenKey);
+            DeleteToken(RefreshTokenKey);
+        }
+
+        private void SaveToken(string key, string token)
+        {
+            using var cred = new Credential
+            {
+                Target = key,
+                Username = "Token",
+                Password = token,
+                PersistanceType = PersistanceType.LocalComputer
+            };
+            cred.Save();
+        }
+
+        private string? GetToken(string key)
+        {
+            using var cred = new Credential { Target = key };
+            return cred.Load() ? cred.Password : null;
+        }
+
+        private void DeleteToken(string key)
+        {
+            using var cred = new Credential { Target = key };
+            cred.Delete();
+        }
+    }
+}
