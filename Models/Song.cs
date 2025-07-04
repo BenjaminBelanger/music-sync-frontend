747c0759a033a3d01778d21a4e5ecf074bf2d250
Benjamin Belanger <99574524+BenjaminBelanger@users.noreply.github.com>
2025-07-04 00:33:30 -0400

    Add song model (#14)

---
+++ Models/Song.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_sync_frontend.Models
+{
+    public class Song
+    {
+        private string albumId;
+        private int position;
+        private int time;
+        private int length;
+
+        public Song(string albumId, int position, int time)
+        {
+            this.albumId = albumId;
+            this.position = position;
+            this.time = time;
+        }
+
+        public Song(string albumId, int position, int time, int length)
+        {
+            this.albumId = albumId;
+            this.position = position;
+            this.time = time;
+            this.length = length;
+        }
+
+        public string AlbumId
+        {
+            get { return albumId; }
+            set { albumId = value; }
+        }
+
+        public int Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"AlbumId={AlbumId}, Position={Position}, Time={Time}";
+        }
+    }
+}
