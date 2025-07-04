bef7c3e64bd80e7857d5484efbff61d1af8b86cd
BenjaminBelanger <benjamin-belanger@hotmail.com>
2025-07-03 23:54:27 -0400

    Add refresh token storage

--- Services/TokenStorageService.cs
+++ Services/TokenStorageService.cs
@@ -1,36 +1,107 @@
 ﻿using CredentialManagement;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace music_sync_frontend.Services
 {
     public class TokenStorageService
     {
-        public void SaveToken(string key, string token)
+        private const string AccessTokenSuffix = "_access";
+        private const string RefreshTokenSuffix = "_refresh";
+
+        public void SaveTokens(string key, string accessToken, string refreshToken)
+        {
+            using var accessCred = new Credential
+            {
+                Target = key + AccessTokenSuffix,
+                Username = "SpotifyAccessToken",
+                Password = accessToken,
+                PersistanceType = PersistanceType.LocalComputer
+            };
+            accessCred.Save();
+
+            using var refreshCred = new Credential
+            {
+                Target = key + RefreshTokenSuffix,
+                Username = "SpotifyRefreshToken",
+                Password = refreshToken,
+                PersistanceType = PersistanceType.LocalComputer
+            };
+            refreshCred.Save();
+        }
+
+        public (string? AccessToken, string? RefreshToken) GetTokens(string key)
+        {
+            string? accessToken = null;
+            string? refreshToken = null;
+
+            using (var accessCred = new Credential { Target = key + AccessTokenSuffix })
+            {
+                if (accessCred.Load())
+                {
+                    accessToken = accessCred.Password;
+                }
+            }
+
+            using (var refreshCred = new Credential { Target = key + RefreshTokenSuffix })
+            {
+                if (refreshCred.Load())
+                {
+                    refreshToken = refreshCred.Password;
+                }
+            }
+
+            return (accessToken, refreshToken);
+        }
+
+        public void DeleteTokens(string key)
+        {
+            using (var accessCred = new Credential { Target = key + AccessTokenSuffix })
+            {
+                accessCred.Delete();
+            }
+
+            using (var refreshCred = new Credential { Target = key + RefreshTokenSuffix })
+            {
+                refreshCred.Delete();
+            }
+        }
+
+        public void SaveAccessToken(string key, string token)
+        {
+            SaveToken(key + AccessTokenSuffix, token, "SpotifyAccessToken");
+        }
+
+        public void SaveRefreshToken(string key, string token)
+        {
+            SaveToken(key + RefreshTokenSuffix, token, "SpotifyRefreshToken");
+        }
+
+        public string? GetAccessToken(string key)
+        {
+            return GetToken(key + AccessTokenSuffix);
+        }
+
+        public string? GetRefreshToken(string key)
+        {
+            return GetToken(key + RefreshTokenSuffix);
+        }
+
+        private void SaveToken(string fullKey, string token, string username)
         {
             using var cred = new Credential
             {
-                Target = key,
-                Username = "SpotifyAuthToken",
+                Target = fullKey,
+                Username = username,
                 Password = token,
                 PersistanceType = PersistanceType.LocalComputer
             };
             cred.Save();
         }
 
-        public string? GetToken(string key)
+        private string? GetToken(string fullKey)
         {
-            using var cred = new Credential { Target = key };
+            using var cred = new Credential { Target = fullKey };
             return cred.Load() ? cred.Password : null;
-        }
-
-        public void DeleteToken(string key)
-        {
-            using var cred = new Credential { Target = key };
-            cred.Delete();
         }
     }
 }
