0447547697859716b90e148600ffc2a48bbe856c
BenjaminBelanger <benjamin-belanger@hotmail.com>
2025-07-03 23:53:24 -0400

    Add token storage

---
+++ Services/ITokenStorageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_sync_frontend.Services
+{
+    public interface ITokenStorageService
+    {
+        void SaveToken(string key, string token);
+        string? GetToken(string key);
+        void DeleteToken(string key);
+    }
+}
---
+++ Services/TokenStorageService.cs
@@ -0,0 +1,36 @@
+﻿using CredentialManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_sync_frontend.Services
+{
+    public class TokenStorageService
+    {
+        public void SaveToken(string key, string token)
+        {
+            using var cred = new Credential
+            {
+                Target = key,
+                Username = "SpotifyAuthToken",
+                Password = token,
+                PersistanceType = PersistanceType.LocalComputer
+            };
+            cred.Save();
+        }
+
+        public string? GetToken(string key)
+        {
+            using var cred = new Credential { Target = key };
+            return cred.Load() ? cred.Password : null;
+        }
+
+        public void DeleteToken(string key)
+        {
+            using var cred = new Credential { Target = key };
+            cred.Delete();
+        }
+    }
+}
